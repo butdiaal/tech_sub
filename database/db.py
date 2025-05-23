de26dbdf2fc5fb5a23f3899e2353a99174645088
butdiaal <dianabutko2007@gmail.com>
2025-05-23 09:04:46 +0300

    добавлено подключение

--- database/db.py
+++ database/db.py
@@ -1,2 +1,18 @@
-import MySQLdb
-  #LALALAL
+import MySQLdb as mdb
+
+def get_db_connection():
+    """Создает соединение с базой данных."""
+    try:
+        conn = mdb.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="tech_sub",
+            autocommit = True
+        )
+        print('Connection ok')
+        conn.close()
+
+    except Exception as ex:
+        print('Connection no')
+        print(ex)
