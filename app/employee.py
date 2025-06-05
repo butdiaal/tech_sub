399a6fc34efbc9839ede9363efef960a4d567a3c
aganchoglo <ganchoglogithub@gmail.com>
2025-06-05 10:37:37 +0300

    окно имплоии мэйн

---
+++ app/employee.py
@@ -0,0 +1,32 @@
+from PyQt6 import *
+from app.graf.employees_graf import *
+
+
+class Employee_Window(QtWidgets, Employee_Ui_Form):
+    def __init__(self, parent=None, employee_id = None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.employee_id = employee_id
+        # self.init_ui()
+
+        self.layout_types = QtWidgets.QVBoxLayout(self.groupBox_types)
+        self.layout_status = QtWidgets.QVBoxLayout(self.groupBox_status)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    win = Employee_Ui_Form()
+    win.show()
+    sys.exit(app.exec())
