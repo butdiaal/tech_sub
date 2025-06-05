4dbd19237112bbbcae8a1b875eeafba6b11e13e8
aganchoglo <ganchoglogithub@gmail.com>
2025-06-05 15:25:25 +0300

    окно имплоии мэйн

--- app/employee.py
+++ app/employee.py
@@ -1,9 +1,10 @@
 from PyQt6 import *
 from app.graf.employees_graf import *
-from database.db import *
+import database.db as db
 
-class Employee_Window(QtWidgets, Employee_Ui_Form):
-    def __init__(self, parent=None, employee_id = None):
+
+class Employee_Window(QtWidgets.QWidget, Employee_Ui_Form):
+    def __init__(self, parent=None, employee_id=None):
         super().__init__(parent)
         self.setupUi(self)
         self.employee_id = employee_id
@@ -12,25 +13,18 @@ class Employee_Window(QtWidgets, Employee_Ui_Form):
         self.layout_types = QtWidgets.QVBoxLayout(self.groupBox_types)
         self.layout_status = QtWidgets.QVBoxLayout(self.groupBox_status)
 
-
-        types =
+        types = db.get_types()
+        self.radios_types = []
         for type in types:
-
-
-
-
-
-
-
-
-
-
+            radio_types = QtWidgets.QRadioButton(type[0])
+            self.layout_types.addWidget(radio_types)
+            self.radios_types.append(radio_types)
 
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    win = Employee_Ui_Form()
+    win = Employee_Window()
     win.show()
     sys.exit(app.exec())
