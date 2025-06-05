f3401435fcea664bbad8f44baaebd0376783c327
MaryKurbatova <138988843+MaryKurbatova@users.noreply.github.com>
2025-06-06 00:14:57 +0300

    yes

--- app/employee.py
+++ app/employee.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtCore
 from app.graf.employees_graf import *
 import database.db as db
 
@@ -32,6 +32,9 @@ class Employee_Window(QtWidgets.QWidget, Employee_Ui_Form):
 
         # Загрузка тикетов
         self.load_tickets()
+
+        # Подключаем кнопку take_tickets
+        self.take_ticket_bt.clicked.connect(self.on_take_ticket_clicked)
 
     def load_types(self):
         types = db.get_types()
@@ -72,6 +75,7 @@ class Employee_Window(QtWidgets.QWidget, Employee_Ui_Form):
         # Настройки таблицы
         self.tickets_table.setRowCount(len(tickets))
         self.tickets_table.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.tickets_table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectionBehavior.SelectRows)
 
         # Уменьшаем шрифт
         font = self.tickets_table.font()
@@ -88,22 +92,59 @@ class Employee_Window(QtWidgets.QWidget, Employee_Ui_Form):
                 # Включаем перенос текста для столбца Description (индекс 3)
                 if col_idx == 3:  # description
                     item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignLeft | QtCore.Qt.AlignmentFlag.AlignTop)
-                    item.setFlags(item.flags() | QtCore.Qt.ItemFlag.ItemIsAutoTristate)  # разрешаем перенос
+                    item.setFlags(item.flags() | QtCore.Qt.ItemFlag.ItemIsAutoTristate)
                 self.tickets_table.setItem(row_idx, col_idx, item)
 
         # Настраиваем поведение столбцов
-        self.tickets_table.horizontalHeader().setSectionResizeMode(3,
-                                                                   QtWidgets.QHeaderView.ResizeMode.Stretch)  # description растягивается
-        self.tickets_table.setWordWrap(True)  # включаем перенос слов
-        self.tickets_table.resizeRowsToContents()  # подгоняем высоту строк под текст
+        self.tickets_table.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeMode.Stretch)
+        self.tickets_table.setWordWrap(True)
+        self.tickets_table.resizeRowsToContents()
 
         # Остальные столбцы подгоняем по содержимому
-        for col in [0, 1, 2, 4, 5, 6]:  # кроме description (3)
+        for col in [0, 1, 2, 4, 5, 6]:
             self.tickets_table.horizontalHeader().setSectionResizeMode(col,
                                                                        QtWidgets.QHeaderView.ResizeMode.ResizeToContents)
 
         # Добавляем таблицу в layout
         self.layout_tickets.addWidget(self.tickets_table)
+
+    def on_take_ticket_clicked(self):
+        """Обработчик нажатия кнопки взять заявку"""
+        selected_items = self.tickets_table.selectedItems()
+
+        if not selected_items:
+            QtWidgets.QMessageBox.warning(self, "Ошибка", "Выберите заявку из таблицы!")
+            return
+
+        # Получаем ID выбранного тикета (первый столбец в строке)
+        selected_row = selected_items[0].row()
+        ticket_id_item = self.tickets_table.item(selected_row, 0)  # столбец ID
+        ticket_id = int(ticket_id_item.text())
+
+        # Получаем текущий статус заявки
+        status_item = self.tickets_table.item(selected_row, 4)  # столбец Status
+        current_status = status_item.text().lower()
+
+        if current_status != 'в ожидании':
+            QtWidgets.QMessageBox.warning(self, "Ошибка", "Можно брать только заявки со статусом 'В ожидании'!")
+            return
+
+        # Вызываем функцию take_ticket
+        if db.take_ticket(ticket_id, self.employee_id):
+            # Обновляем статус в таблице
+            status_item.setText("в работе")
+
+            # Обновляем информацию о сотруднике (если есть соответствующий столбец)
+            employee_item = self.tickets_table.item(selected_row, 6)  # столбец Employee
+            if employee_item:
+                # Здесь нужно получить ФИО текущего сотрудника (добавьте соответствующий метод в db)
+                employee_name = db.get_employee_name(self.employee_id)
+                if employee_name:
+                    employee_item.setText(employee_name)
+
+            QtWidgets.QMessageBox.information(self, "Успех", "Заявка успешно взята в работу!")
+        else:
+            QtWidgets.QMessageBox.warning(self, "Ошибка", "Не удалось взять заявку!")
 
 
 if __name__ == "__main__":
