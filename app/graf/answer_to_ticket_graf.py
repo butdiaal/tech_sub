985a5c055a1e4e07145de64c65175cbaf55f3b7f
aganchoglo <ganchoglogithub@gmail.com>
2025-06-06 09:23:48 +0300

    добавила окно и немного изменила ддля отвекта

--- app/graf/answer_to_ticket_graf.py
+++ app/graf/answer_to_ticket_graf.py
@@ -9,7 +9,7 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Form(object):
+class Answer_Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(509, 657)
@@ -250,7 +250,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = Ui_Form()
+    ui = Answer_Ui_Form()
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec())
